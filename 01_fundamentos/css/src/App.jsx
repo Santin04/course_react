16ce7e17bb30443d62c8f80a48730609b4a7a3f4
Santin04 <masantinte@gmail.com>
2024-09-13 09:42:53 -0300

    css conditional, class conditional and css module

--- 01_fundamentos/css/src/App.jsx
+++ 01_fundamentos/css/src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
 import MyComponent from './components/MyComponent'
-// import './App.css'
+import './App.css'
+import { useState } from 'react';
+import CssModule from './components/CssModule';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const num = 15;
+  const [name, setName] = useState('Mateus');
+
+  const editColor = () => {
+    setName('Mike');
+  };
+
+  const MyClass = true;
 
   return (
     <>
       <MyComponent />
       {/* Mostrando um exemplo de como colocar css sem o arquivo css */}
       <p style={{ color: 'blue' }}>Css diretamente do HTML</p>
-    </>
-  )
-}
 
-export default App
+      {/* Css inline com condicional */}
+      <h2 style={num < 10 ? ({color: "purple"}) : ({color: "green"})}>
+        Teste de css com condicional
+      </h2>
+
+      {/* Css inline com condicional e dinamico */}
+      <h2 style={name == 'Mateus' ? ({color: "greenyellow"}) : (null)}>
+        {name}
+      </h2>
+      <button onClick={editColor}>Editar nome</button>
+
+      {/* Deixando as classes de forma dinamica com condicionais */}
+      <h2 className={MyClass ? ('classe01') : ('classe02')}>
+        Testando classes
+      </h2>
+
+      {/* CSS module */}
+      <CssModule />
+    </>
+  );
+};
+
+export default App;
---
+++ 01_fundamentos/css/src/components/CssModule.jsx
@@ -0,0 +1,12 @@
+import styles from "./CssModule.module.css";
+
+const CssModule = () => {
+
+    return (
+        <>
+            <h1 className={styles.My_Class_Module}>Testando css module</h1>
+        </>
+    );
+};
+
+export default CssModule;
