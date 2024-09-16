0dd2036c1dbd40d9be0d470f8fd259be78d57171
Santin04 <masantinte@gmail.com>
2024-09-15 22:32:53 -0300

    form 02

--- 01_fundamentos/forms/src/App.jsx
+++ 01_fundamentos/forms/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MyForm01 from "./components/MyForm01";
+import MyForm02 from "./components/MyForm02";
 
 function App() {
     return (
         <>
             <MyForm01 />
+            <MyForm02 />
         </>
     );
 }
--- 01_fundamentos/forms/src/components/MyForm02.jsx
+++ 01_fundamentos/forms/src/components/MyForm02.jsx
@@ -4,13 +4,26 @@ const MyForm02 = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
 
-    const handleName = (e) => {
-        console.log("Mudou o nome");
+    const editName = (e) => {
+        // o "e" é o evento e tem várias propriedades e da para acessar o valor
+        // de dentro do input através desse seguinte código
+        console.log(e.target.value);
     };
 
     return (
         <>
-            <form></form>
+            <form>
+                <label htmlFor="name">Nome:</label>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Digite seu nome"
+                    onChange={editName}
+                />
+                {/* o onchange faz que toda vez que o valor do input seja
+                alterado (incluir ou retirar um caracter) vai realizar a
+                função */}
+            </form>
         </>
     );
 };
