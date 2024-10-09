18336a71e903dc5db31cf756093efa2d5a04e7b8
Santin04 <masantinte@gmail.com>
2024-10-09 20:42:44 -0300

    finish complex context

--- 04_contextAPI/context/src/context/TitleColorContext.jsx
+++ 04_contextAPI/context/src/context/TitleColorContext.jsx
@@ -2,7 +2,16 @@ import { createContext, useReducer } from "react";
 
 export const TitleColorContext = createContext();
 
-export const titleColorReducer = (state, action) => {};
+export const titleColorReducer = (state, action) => {
+    switch (action.type) {
+        case "RED":
+            return { ...state, color: "red" };
+        case "BLUE":
+            return { ...state, color: "blue" };
+        default:
+            return state;
+    }
+};
 
 export const TitleColorContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(titleColorReducer, {
@@ -11,5 +20,9 @@ export const TitleColorContextProvider = ({ children }) => {
 
     console.log("Title color context: ", state);
 
-    return <TitleColorContext>{children}</TitleColorContext>;
+    return (
+        <TitleColorContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </TitleColorContext.Provider>
+    );
 };
--- 04_contextAPI/context/src/pages/Home.jsx
+++ 04_contextAPI/context/src/pages/Home.jsx
@@ -1,14 +1,34 @@
 import { useContext } from "react";
 import { CounterContext } from "../context/CounterContext";
+import { useTitleColor } from "../hooks/useTitleColor";
 
 const Home = () => {
     const { counter, setCounter } = useContext(CounterContext);
 
+    const { color, dispatch } = useTitleColor();
+    const setTitleColor = (color) => {
+        dispatch({ type: color });
+    };
+
     return (
         <div>
-            <h1>This is Home</h1>
+            <h1 style={{ color: color }}>This is Home</h1>
             <p>Valor do contador: {counter}</p>
             <button onClick={() => setCounter(counter + 1)}>+ 1</button>
+            <button
+                onClick={() => {
+                    setTitleColor("RED");
+                }}
+            >
+                Vermelho
+            </button>
+            <button
+                onClick={() => {
+                    setTitleColor("BLUE");
+                }}
+            >
+                Azul
+            </button>
         </div>
     );
 };
